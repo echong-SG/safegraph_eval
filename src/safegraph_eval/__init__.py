0a477e51bcd3e3c5d1010d48ea9b1a0ac321722b
Jeff C. Ho <jeff@safegraph.com>
2021-11-19 11:31:44 -0500

    Add preprocessing and plotting to init script

--- src/safegraph_eval/__init__.py
+++ src/safegraph_eval/__init__.py
@@ -1,2 +1,4 @@
-all = ['ingest']
+all = ['ingest', 'preprocessing', 'plotting']
 from safegraph_eval.ingest import ingest
+from safegraph_eval.preprocessing import preprocessing
+from safegraph_eval.plotting import plotting
